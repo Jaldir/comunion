5f326e65d38ebaaafa714a02cc927ba45704796f
luiscajl <luisca_jl@hotmail.com>
2015-11-03 18:19:56 +0100

    Revert "na"
    This reverts commit f69715bc5e2b4a10edf3369d6fa70b23967afc79.

--- app/src/main/java/com/example/luisca/comunio20/MainPruebas.java
+++ app/src/main/java/com/example/luisca/comunio20/MainPruebas.java
@@ -18,7 +18,6 @@ import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
-import android.R;
 /**
  * Created by Luisca on 3/11/15.
  */
